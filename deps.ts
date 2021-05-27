410452e81d60bc2aa26fd97cf406283bf5123d54
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-05-27 13:00:13 +0530

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Client } from "https://deno.land/x/postgres/mod.ts";
-export type { ConnectionOptions } from "https://deno.land/x/postgres/connection/connection_params.ts";
+export { Client } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
+export type { ConnectionOptions } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
