27a0b4a75e11c8d61a0b2a729be546c3f56f4216
Andrew Morris <voltrevo@gmail.com>
2021-06-21 23:52:05 +0000

    Update postgres to 0.11.3

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Client } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
-export type { ConnectionOptions } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
+export { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
+export type { ConnectionOptions } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
